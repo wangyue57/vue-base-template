e8641336880fbd657b32615ef48ab9b413cea706
王跃 <wangyue57@meituan.com>
2018-06-20 10:46:13 +0800

    移除冗余

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <AnVueComponent/>
-    this is {{date.toISOString()}}
   </div>
 </template>
 
@@ -14,24 +13,6 @@
     name: 'Home',
     mixins: [vuexMixin],
     components: {AnVueComponent},
-    data: () => ({
-      timer: null,
-      date: new Date
-    }),
-    methods: {
-      refreshDate () {
-        this.date = new Date
-      }
-    },
-    created () {
-      this.refreshDate = this.refreshDate.bind(this)
-    },
-    mounted () {
-      this.timer = setInterval(this.refreshDate, 100)
-    },
-    beforeDestroy () {
-      clearInterval(this.timer)
-    }
   }
 </script>
 
